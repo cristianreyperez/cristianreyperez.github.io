dc367cdb034daccfa2a48d8331a3f6209f1148d7
cristianreyperez <perezcristianrey@gmail.com>
2026-01-08 23:13:24 +0800

    moved to root the build files

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/cristianreyperez.github.io/', // or '/' if deploying to root
+  build: {
+    outDir: 'root'
+  }
 })
